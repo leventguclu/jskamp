c56d1564351835befd94fb25b5cadda6fb8c4774
leventguclu <leventtguclu@gmail.com>
2021-10-19 00:42:39 +0300

    18 Ekim Derssonu

---
+++ src/app.js
@@ -0,0 +1,17 @@
+let konutKredileri = ["Konut Kredisi", "Emlak Kredisi", "Kamu Konut Kredisi", "Özel Konut"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+// for 
+// {/* <ul>
+//     <li>Konut Kredisi</li>
+//     <li>Emlak Kredisi</li>
+//     <li>Kamu Konut Kredisi</li>
+// </ul> */}
+
+// console.log(konutKredileri)
